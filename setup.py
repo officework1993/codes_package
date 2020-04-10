f82e547b2c8a7ba44590182d2479ae5ca0c6070e
inder <chahal.inder0007@gmail.com>
2020-04-10 17:22:54 +0530

    name changed to orignal

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
 	version = "3.1.1",
 	description="This is just a test package for python install",
 	url="git@github.com:officework1993/codes_package.git",
-	author="chahal.inder0007@gmail.com",
-	packages="tensorflow",
+	author="chahal.inder0007",
+        author_email="chahal.inder0007@gmail.com",
+	packages="[tensorflow"],
 	license = "unlicense",
 	zip_safe=False)
