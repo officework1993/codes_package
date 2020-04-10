443a0b00952552df67450690f66f2c2008075ead
inder <chahal.inder0007@gmail.com>
2020-04-10 17:12:26 +0530

    initial commit to test

---
+++ all_codes/__init__.py
@@ -0,0 +1,5 @@
+def my_test_function():
+    print("I hope it works")
+
+# import all else here as well
+import tensorflow
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name = "codes",
+	version = "3.1.1",
+	description="This is just a test package for python install",
+	url="git@github.com:officework1993/codes_package.git",
+	author="chahal.inder0007@gmail.com",
+	packages="tensorflow",
+	license = "unlicense",
+	zip_safe=False)
