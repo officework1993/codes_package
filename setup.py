2cae03348cee9e8721e51c25ee9761b1b222d47a
inder <chahal.inder0007@gmail.com>
2020-04-10 17:37:10 +0530

    name change

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-	name = "all_codes",
+	name = "all-codes",
 	version = "3.1.1",
 	description="This is just a test package for python install",
 	url="git@github.com:officework1993/codes_package.git",
 	author="chahal.inder0007",
     author_email="chahal.inder0007@gmail.com",
-	packages=["all_codes"],
+	packages=["all-codes"],
 	license = "unlicense",
 	zip_safe=False
 	)
